6b03beed733f82477efe817fee3e833d8efcaa47
capartech <sekhar@capartech.com>
2023-12-29 09:15:53 +0000

    updated website content

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -22,7 +22,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps";
+			return "Kubernetes DevSecOps by Capartech for testing";
 		}
 
 		@GetMapping("/compare/{value}")
